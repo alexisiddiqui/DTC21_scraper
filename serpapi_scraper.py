080ebcaa54ffefc05781a5746dab1fa2e50597e4
alexisiddiqui <alexisiddiqui831@gmail.com>
2023-04-02 14:32:22 +0100

    blah

--- serpapi_scraper.py
+++ serpapi_scraper.py
@@ -11,13 +11,13 @@ from urllib.parse import urlsplit, parse_qsl
 import pandas as pd
 
 
-def profile_results(name):
+def profile_results():
     print("Extracting profile results..")
 
     params = {
         "api_key": "...",                     # https://serpapi.com/manage-api-key
         "engine": "google_scholar_profiles",  # profile results search engine
-        "mauthors": name,               # search query
+        "mauthors": "blizzard",               # search query
     }
     search = GoogleSearch(params)
 
